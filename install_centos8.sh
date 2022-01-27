b8498627fa1e4f44baaf34a3d2d49c5ecf25b000
matanwrites <20499416+matanwrites@users.noreply.github.com>
2022-01-27 23:12:52 +0800

    improved bash script

--- install_centos8.sh
+++ install_centos8.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-dnf check-update
-dnf upgrade
+set -ueo pipefail
+set -x
+
+dnf check-update || true
+dnf upgrade -y
 
 dnf update -y
+dnf install -y dnf-plugins-core
+#dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 < packages.txt xargs dnf install -y
+
+
+echo -e "\n DONE"
